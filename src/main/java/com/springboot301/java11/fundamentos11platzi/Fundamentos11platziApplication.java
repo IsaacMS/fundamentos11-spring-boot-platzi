12ffb93e28ca6cd8b242f3c0c48d1acc29e3c8a8
Isaac <isaac_33.1@hotmail.com>
2023-01-02 18:59:36 -0600

    second commit

---
+++ src/main/java/com/springboot301/java11/fundamentos11platzi/Fundamentos11platziApplication.java
@@ -0,0 +1,13 @@
+package com.springboot301.java11.fundamentos11platzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundamentos11platziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundamentos11platziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot301/java11/fundamentos11platzi/Fundamentos11platziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot301.java11.fundamentos11platzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fundamentos11platziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
